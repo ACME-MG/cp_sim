ab8fd74592119c75ce16ac193426b13a3687e732
Janzen <janzenchoi@yahoo.com>
2024-06-03 23:50:54 +1000

    added p91 code

---
+++ scripts/sample_p91.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+nohup python3 sample_p91.py 0 0 &
+nohup python3 sample_p91.py 0 1 &
+nohup python3 sample_p91.py 0 2 &
+nohup python3 sample_p91.py 0 3 &
+
+nohup python3 sample_p91.py 1 0 &
+nohup python3 sample_p91.py 1 1 &
+nohup python3 sample_p91.py 1 2 &
+nohup python3 sample_p91.py 1 3 &
+
+nohup python3 sample_p91.py 2 0 &
+nohup python3 sample_p91.py 2 1 &
+nohup python3 sample_p91.py 2 2 &
+nohup python3 sample_p91.py 2 3 &
+
+nohup python3 sample_p91.py 3 0 &
+nohup python3 sample_p91.py 3 1 &
+nohup python3 sample_p91.py 3 2 &
+nohup python3 sample_p91.py 3 3 &
